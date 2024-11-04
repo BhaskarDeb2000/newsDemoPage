123bb86e4dfd56a305c5e44300fc9a0c5339a818
BhaskarDeb2000 <bhaskardeb120@gmail.com>
2024-11-04 17:20:16 +0200

    feat: frontEnd deployed :rocket:

--- client/src/pages/News.js
+++ client/src/pages/News.js
@@ -3,20 +3,70 @@ import axios from "axios";
 
 const News = () => {
   const [news, setNews] = useState([]);
-  //const [page, setPage] = useState(1);
-  //const [loading, setLoading] = useState(false);
-  //const [error, setError] = useState(null);
-  //const [totalResults, setTotalResults] = useState(0);
-  //const pageSize = 5;
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [page, setPage] = useState(5);
+  const [totalResults, setTotalResults] = useState(0);
+  const pageSize = 5;
 
   useEffect(() => {
     const fetchNews = async () => {
-      const res = await axios.get(`http://localhost:5001/news`);
-      setNews(res.data.articles);
+      setLoading(true);
+      setError(null); // Reset error on new request
+      try {
+        const response = await axios.get(
+          `https://news-demo-backend-api.vercel.app/news?page=${page}&pageSize=${pageSize}`
+        );
+        setNews(response.data.articles);
+        setTotalResults(response.data.totalResults);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
     };
-    fetchNews();
-  }, []);
 
-  return <h1>News</h1>;
+    fetchNews();
+  }, [page]);
+
+  const nextPage = () => {
+    if (page < Math.ceil(totalResults / pageSize)) setPage(page + 1);
+  };
+
+  const prevPage = () => {
+    if (page > 1) setPage(page - 1);
+  };
+
+  return (
+    <div>
+      <div>
+        {error ? (
+          <p>{error}</p>
+        ) : loading ? (
+          <p>Loading...</p>
+        ) : (
+          news.map((i, index) => (
+            <div key={index}>
+              <ul>
+                <li>{i.title}</li>
+              </ul>
+            </div>
+          ))
+        )}
+      </div>
+      <div className="pagination">
+        <button onClick={prevPage} disabled={page === 1}>
+          &larr; Previous
+        </button>
+        <span>Page {page}</span>
+        <button
+          onClick={nextPage}
+          disabled={page >= Math.ceil(totalResults / pageSize)}
+        >
+          Next &rarr;
+        </button>
+      </div>
+    </div>
+  );
 };
 export default News;
